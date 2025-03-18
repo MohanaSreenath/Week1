e7d48111871fc21e1240c2c68ece96026de37769
MohanaSreenath <mohanasreenathk@gmail.com>
2025-03-18 12:00:00 +0530

    Practice Problems

--- Day1_Programming_Elements/lev_1pp/Fee.java
+++
@@ -1,8 +0,0 @@
-class Fee{
-    public static void main(String[] args){
-        int fee = 125000 ,discount = 10;
-        int discount_amount = fee*discount/100;
-        int discountedFee = fee - discount_amount;
-        System.out.println("The discount amount is INR "+discount_amount+" and final discounted fee is INR"+discountedFee);
-    }
-}
--- Day1_Programming_Elements/lev_2pp/Tempconv.java
+++
@@ -1,8 +0,0 @@
-class Tempconv{
-    public static void main(String[] args){
-        double celcuis,fahrenheit;
-        celcuis=37.0;
-        fahrenheit=(celcuis*9/5)+32;
-        System.out.println("The "+celcuis+" celsius is "+fahrenheit+" fahrenheit");
-    }
-}
