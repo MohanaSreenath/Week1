a437f8b7218a2167528d81d740ec8463cef37ec8
MohanaSreenath <mohanasreenathk@gmail.com>
2025-03-24 12:00:00 +0530

    String arrays and Renamed all mistake  Directories

---
+++ Day6_String_Extras/Duplicates.java
@@ -0,0 +1,20 @@
+package Extras;
+
+public class Duplicates {
+    public static String removeDuplicates(String input) {
+        StringBuilder result = new StringBuilder();
+        boolean[] seen = new boolean[256];
+        for (char c : input.toCharArray()) {
+            if (!seen[c]) {
+                seen[c] = true;
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "programming";
+        System.out.println(removeDuplicates(input));
+    }
+}
---
+++ Day6_String_Extras/Remove.java
@@ -0,0 +1,10 @@
+package Extras;
+
+public class Remove {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        char charToRemove = 'l';
+        String result = input.replace(Character.toString(charToRemove), "");
+        System.out.println("Modified String: " + result);
+    }
+}
